49ebfb34c418626cbc0b7f59f809aeb3a3ff8452
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-06-21 17:06:56 +0300

    created Table component with style and logic

--- src/components/molecules/Table/Table.tsx
+++ src/components/molecules/Table/Table.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const Table = () => {
-  return (
-    <div>Table</div>
-  )
+import { StyledTable, StyledHeadlineRow, StyledRow, StyledHeadlineContainer, StyledDataContainer, } from './styles';
+
+export interface IRocket {
+  id: number;
+  cost_per_launch: number;
+  height: {
+    meters: number;
+    feet: number;
+  };
+  diameter: {
+    meters: number;
+    feet: number;
+  };
+  mass: {
+    kg: number;
+    lb: number;
+  };
+  description: string;
+  rocket_id: string;
+  rocket_name: string;
+  rocket_type: string;
 }
 
-export default Table
+// interface ISearchData {
+//   headlines: string[];
+//   rockets: IRocket[];
+// }
+
+// interface ISeatchProps {
+//   data: ISearchData;
+// }
+
+interface ITableProps {
+  headline: string[];
+  rockets: IRocket[];
+}
+
+const Table = ({headline, rockets}: ITableProps) => {
+  // const [searchValue, setSearchValue] = useState('');
+
+  // const filteredRockets = props.rockets.filter((rocket) =>
+  //   rocket.rocket_name.toLowerCase().includes(searchValue.toLowerCase())
+  // );
+
+  return (
+    <StyledTable>
+      <StyledHeadlineRow>
+        {headline.map((headline, index) => (
+          <StyledHeadlineContainer key={index} className='table-header'>
+            <h6>{headline}</h6>
+          </StyledHeadlineContainer>
+        ))}
+      </StyledHeadlineRow>
+      {rockets.map((rocket, index) => (
+        <StyledRow key={index}>
+          <StyledDataContainer>
+            <p>{rocket.rocket_name}</p>
+          </StyledDataContainer>
+          <StyledDataContainer>
+            <p>{rocket.diameter.meters}m</p>
+          </StyledDataContainer>
+          <StyledDataContainer>
+            <p>{rocket.height.meters}m</p>
+          </StyledDataContainer>
+          <StyledDataContainer>
+            <p>{rocket.mass.kg}kg</p>
+          </StyledDataContainer>
+          <StyledDataContainer>
+            <p>{rocket.cost_per_launch}</p>
+          </StyledDataContainer>
+        </StyledRow>
+      ))}
+    </StyledTable>
+  );
+};
+
+export default Table;
