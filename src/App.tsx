6996ebe79847436db18a57d9714affd843a39f4c
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-06-21 17:04:13 +0300

    updated App.ts added comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-
-import './App.css';
 import React from 'react';
 import { API } from './shared/api';
 import SearchTable from './components/organisims/SearchTable';
 
 
 function App() {
-  React.useEffect(() => {
-    API.getProducts()
-      .then((data: any) => {
-        console.log(data);
-      })
-      .catch((error: any) => {
-        console.error(error);
-      });
-  }, []);
+  // React.useEffect(() => {
+  //   API.getProducts()
+  //     .then((data: any) => {
+  //       console.log(data);
+  //     })
+  //     .catch((error: any) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   
 
