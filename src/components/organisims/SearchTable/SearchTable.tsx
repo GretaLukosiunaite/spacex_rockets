5f8cb8954ac5fe1cfb8a59db9fc2168baf134209
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-06-21 17:08:13 +0300

    created a little logic of SearchTable component and in styled.css added syled components

--- src/components/organisims/SearchTable/SearchTable.tsx
+++ src/components/organisims/SearchTable/SearchTable.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchFilter from '../../molecules/Search';
 import Table from '../../molecules/Table';
+import { API } from '../../../shared/api';
+
+const tableHeadline = [
+    'Rocket name',
+    'Diameter',
+    'Height',
+    'Mass',
+    'Cost per launch',
+  ];
+  
 
 const SearchTable = () => {
+  const [rockets, setRockets] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await API.getProducts();
+        setRockets(data);
+      } catch (error) {
+        console.error('Error fetching rocket data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <SearchFilter />
-      <Table />
+      {rockets.length > 0 ? (
+        <Table rockets={rockets} headline={tableHeadline}/>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
