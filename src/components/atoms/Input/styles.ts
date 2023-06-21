5ed2bc17b55173f765e249a5e4e4e7bb24550374
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-06-21 10:54:24 +0300

    added style for input atom in syles.ts

---
+++ src/components/atoms/Input/styles.ts
@@ -0,0 +1,36 @@
+import { styled } from 'styled-components';
+
+export const SyledInputDiv = styled.div`
+  display: flex;
+  align-items: center;
+
+  background-color: #f5f5fa;
+  border-radius: 50px;
+
+  width: 753px;
+  padding: 10px 0 9px 15px;
+`;
+
+export const SyledIcon = styled.span`
+  color: #5a71e4;
+  font-size: 14px;
+`;
+
+export const SyledInput = styled.input`
+  width: calc(100% - 3%);
+  outline: none;
+  border: none;
+  padding-left: 13px;
+
+  font-size: 1rem;
+  line-height: 100%;
+  font-weight: 400;
+  letter-spacing: 0.5px;
+  color: #9b9eac;
+
+  background-color: #f5f5fa;
+
+  &::placeholder {
+    color: #9b9eac;
+  }
+`;
