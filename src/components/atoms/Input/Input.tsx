0bf2974d7ae1f88fbb1e44db1fb3dadc224accde
Greta Lukošiūnaitė <greta.lukosiunaite@outlook.com>
2023-06-21 11:06:11 +0300

    Input.tsx added interface IINputProps and updated component

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { SyledIcon, SyledInput, SyledInputDiv } from './styles';
 
-const Input = () => {
-  return (
-    <div>input</div>
-  )
+interface IInputProps {
+  type: 'text' | 'number';
+  value: string | number;
+  setvalue: React.Dispatch<React.SetStateAction<string>>;
+  icon: any;
+  placeholder?: string;
 }
 
-export default Input
+const Input = ({ type, value, setvalue, icon, placeholder }: IInputProps) => {
+  return (
+    <SyledInputDiv>
+      <SyledIcon>{icon}</SyledIcon>
+      <SyledInput
+        type={type}
+        value={value}
+        onChange={(e) => setvalue(e.target.value)}
+        placeholder={placeholder ? placeholder : ''}
+      />
+    </SyledInputDiv>
+  );
+};
+
+export default Input;
